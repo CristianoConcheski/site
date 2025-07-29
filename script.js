cdcd258bf18aeed91c83e6c851a9bf85e7f5becd
cristiano Concheski <cris.concheski@gmail.com>
2025-07-29 13:52:45 -0300

    at

--- script.js
+++ script.js
@@ -58,4 +58,21 @@ menuToggle.addEventListener("click", () => {
 });
 
 
+const box = document.querySelector(".container");
+const imagens = document.querySelectorAll(".container img");
 
+let contador = 0;
+
+function slider() {
+  contador++;
+
+  if (contador > imagens.length - 1) {
+    contador = 0;
+  }
+
+  box.style.transform = `translateX(${-contador * 1250}px)`;
+}
+
+setInterval(slider, 0,2);
+
+console.log("Slider iniciado");
